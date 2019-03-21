1fd806be3dafda04020586c2645f1c8c390b0816
Joseph <jappleton@csumb.edu>
2019-03-21 00:50:03 +0000

    models and views filled out

--- online_lj_ilp/config/routes.rb
+++ online_lj_ilp/config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root for site
+  root 'login#index'
+  
+  # login page
+  get 'login/index' => 'login#index'
+  post 'login/index' => 'login#index'
+  
+  # login service and redirect to dash
+  get 'login/sign_in' => 'login#sign_in'
+  post 'login/sign_in' => 'login#sign_in'
+  
+  # after log in landing page
+  get 'member/dashboard' => 'member#dashboard'
+  post 'member/dashboard' => 'member#dashboard'
+  
+  # must be at bottom
+  # catch all
+  get '*path', to: 'login#index'
+
 end
